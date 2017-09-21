6e2266dd72c350ba4c604df8bfe083b6b9aa130a
darkearl <ttrung.david@gmail.com>
2017-09-21 15:11:58 +0700

    init new structure

---
+++ src/components/FilterRoute.js
@@ -0,0 +1,46 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default function createComponent(store, privateRoute=true){
+
+    const isLoggedIn = store.getState().user.isAuthenticated;
+    const requireByPass = privateRoute ? privateRoute && isLoggedIn : !isLoggedIn;
+
+    class FilterComponent extends Component {
+        static propTypes = {
+            router: PropTypes.object.isRequired
+        }
+        constructor(props){
+            super(props);
+            this.state = {
+                currentURL : props.location.pathname
+            }
+        }
+        componentDidMount() {
+            const { router } = this.props
+            console.log(requireByPass,privateRoute)
+            if(!requireByPass){
+                if(privateRoute){
+                    router.replace({
+                        pathname: '/login',
+                        state: { nextPathname: this.state.currentURL }
+                    })
+                }else{
+                    router.replace({
+                        pathname: '/logout',
+                        state: { nextPathname: this.state.currentURL }
+                    })
+                }
+            }
+        }
+        render() {
+            if (requireByPass) {
+                return this.props.children
+            } else {
+                return null
+            }
+        }
+    }
+
+    return FilterComponent;
+}
